bb10246137094a6261f939ecbd96677b8effc970
Dan <d.irinkovn1@gmail.com>
2020-08-09 16:34:33 +0300

    adding delay to requests

--- src/adapters/abstract.js
+++ src/adapters/abstract.js
@@ -161,7 +161,8 @@ class AbstractAdapter {
       })
     }
 
-    items.map(async (item) => {
+    for (let item of items) {
+    // items.map(async (item) => { // NOT WORKING WITH DELAY
       console.log('Dalaying script: ', await sleep(150)) //Having issues with lost packets on my GKE kubernetes cluster, when this script fires 50+ requests instantly. They never reach the ES pod. Without any error
       this.preProcessItem(item).then((item) => {
         this.tasks_count--;
@@ -226,7 +227,7 @@ class AbstractAdapter {
         logger.error(reason);
         return this.onDone(this);
       });
-      })
+      }
   }
 }
 
